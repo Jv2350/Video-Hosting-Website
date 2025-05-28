8af914e44a296cafb94bc6ae9a67154897af1c9a
Jv2350 <tp8375735@gmail.com>
2025-05-29 00:49:09 +0530

    set up database connection and environment configuration

---
+++ src/db/index.js
@@ -0,0 +1,20 @@
+import mongoose, { connect } from "mongoose";
+import { DB_NAME } from "../constant.js";
+
+const MONGO_URI = process.env.MONGO_URI;
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${MONGO_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\nMongo connected Successfully, DB host: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.error("Mongo connection error: ", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log("Hello, World!");
+//R FIRST APPROACH
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constant";
+// import express from "express";
+
+// const app = express();
+// const PORT = process.env.PORT || 3000
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("Error: ", error);
+//       throw error;
+//     });
+
+//     app.listen(PORT,()=>{
+// console.log(`Server is running at http://localhost:${PORT}`);
+
+//     })
+//   } catch (error) {
+//     console.error("Error connect with the data base: ", error);
+//     throw error;
+//   }
+// })();
+
+// -------------------------------------------------------------------
+// SECOND APPROACH
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
