3cbc23fb5455b14ab603a603a74e31fa0a97320e
Jv2350 <tp8375735@gmail.com>
2025-06-15 22:44:58 +0530

    implement channel stats retrieval in dashboard controller

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -7,7 +7,71 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
-  // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+  const userId = req.user?._id;
+
+  if (!userId) {
+    throw new ApiError(400, "Unauthorized request");
+  }
+
+  // Aggregate pipeline to get total video views
+  const totalVideoViews = await Video.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        totalViews: {
+          $sum: "$views",
+        },
+      },
+    },
+  ]);
+
+  // Get total subscribers
+  const totalSubscribers = await Subscription.countDocuments({
+    channel: userId,
+  });
+
+  // Get total videos
+  const totalVideos = await Video.countDocuments({
+    owner: userId,
+  });
+
+  // Get total likes on all videos
+  const totalLikes = await Like.aggregate([
+    {
+      $lookup: {
+        from: "videos",
+        localField: "video",
+        foreignField: "_id",
+        as: "videoDetails",
+      },
+    },
+    {
+      $match: {
+        "videoDetails.owner": new mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $count: "totalLikes",
+    },
+  ]);
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        totalViews: totalVideoViews[0]?.totalViews || 0,
+        totalSubscribers,
+        totalVideos,
+        totalLikes: totalLikes[0]?.totalLikes || 0,
+      },
+      "Channel stats fetched successfully"
+    )
+  );
 });
 
 const getChannelVideos = asyncHandler(async (req, res) => {
